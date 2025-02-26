060e1611951dfab725978cf25b1393c4ab01d852
Ayesha Deshwali <139254189+AyeshaDeshwali@users.noreply.github.com>
2025-02-26 16:01:15 +0530

    Update Navbar.jsx

--- Shopper-clothing/front-end/shopper/src/Components/Navbar/Navbar.jsx
+++ Shopper-clothing/front-end/shopper/src/Components/Navbar/Navbar.jsx
@@ -7,60 +7,75 @@ import { ShopContext } from "../../Context/ShopContext";
 
 const Navbar = () => {
   const [menu, setMenu] = useState("shop");
+  const [menuOpen, setMenuOpen] = useState(false);
+
   const { getTotalCartItems } = useContext(ShopContext);
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <div className="navbar">
       <div className="nav-logo">
-        {/* <img
-          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSmrVEFx_6sdUi685gZXEUzcypy6MBnZ_wHOTlmtGcWxo5Q-1-yeWYQysV9OAmB0DLah6E&usqp=CAU"
-          alt=""
-        /> */}
         <img
           src="https://t3.ftcdn.net/jpg/05/06/75/80/360_F_506758046_vb98YfQhYf6RmwPkgQQH80qPSIoNMYyW.jpg"
-          alt=""
+          alt="logo"
         />
         <p>SHOPPER</p>
       </div>
-      <ul className="nav-menu">
-        <li onClick={() => setMenu("shop")}>
-          <Link style={{ textDecoration: "none" }} to="/">
-            Shop
-          </Link>{" "}
+      <div className="menu-icon" onClick={toggleMenu}>
+        <i className={`fa-solid ${menuOpen ? "fa-xmark" : "fa-bars"}`}></i>
+      </div>
+
+      <ul className={`nav-menu ${menuOpen ? "open" : ""}`}>
+        <li
+          onClick={() => {
+            setMenu("shop");
+            setMenuOpen(false);
+          }}
+        >
+          <Link to="/">Shop</Link>
           {menu === "shop" && <hr />}
         </li>
-        <li onClick={() => setMenu("mens")}>
-          {" "}
-          <Link to="mens" style={{ textDecoration: "none" }}>
-            Men
-          </Link>{" "}
+        <li
+          onClick={() => {
+            setMenu("mens");
+            setMenuOpen(false);
+          }}
+        >
+          <Link to="/mens">Men</Link>
           {menu === "mens" && <hr />}
         </li>
-        <li onClick={() => setMenu("womens")}>
-          <Link to="womens" style={{ textDecoration: "none" }}>
-            Women
-          </Link>{" "}
+        <li
+          onClick={() => {
+            setMenu("womens");
+            setMenuOpen(false);
+          }}
+        >
+          <Link to="/womens">Women</Link>
           {menu === "womens" && <hr />}
         </li>
-        <li onClick={() => setMenu("kids")}>
-          <Link to="kids" style={{ textDecoration: "none" }}>
-            Kids
-          </Link>{" "}
+        <li
+          onClick={() => {
+            setMenu("kids");
+            setMenuOpen(false);
+          }}
+        >
+          <Link to="/kids">Kids</Link>
           {menu === "kids" && <hr />}
         </li>
       </ul>
+
       <div className="nav-login-cart">
-        <Link to="/login" style={{ textDecoration: "none" }}>
-          {" "}
+        <Link to="/login">
           <button>Login</button>
         </Link>
-        <Link to="cart" style={{ textDecoration: "none" }}>
-          {" "}
+        <Link to="/cart">
           <img
             src="https://static.vecteezy.com/system/resources/thumbnails/001/504/962/small/shopping-cart-icon-free-vector.jpg"
-            alt=""
+            alt="cart"
           />
         </Link>
-
         <div className="nav-cart-count">{getTotalCartItems()}</div>
       </div>
     </div>
